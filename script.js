51e9c0854166544974ba9acfa9b7809be74f78ea
anokhimehta <anokhimehta00@gmail.com>
2021-05-02 20:04:13 -0700

    added black rectangle for canvas

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 // script.js
 
 const img = new Image(); // used to load image from <input> and draw to canvas
+const canvas = document.getElementById("user-image");
+const ctx = canvas.getContext('2d');
 
 // Fires whenever the img object loads a new image (such as with img.src =)
 img.addEventListener('load', () => {
   // TODO
-
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
   // Some helpful tips:
   // - Fill the whole Canvas with black first to add borders on non-square images, then draw on top
   // - Clear the form when a new image is selected
